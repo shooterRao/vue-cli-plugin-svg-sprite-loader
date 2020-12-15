c582dbf8f4ff09b5a7a0551c9badcd06f40567df
shooterRao <rjw0319@163.com>
2020-12-15 11:28:16 +0800

    update svg-sprite-loader dependence version(5.x)

--- generator/index.js
+++ generator/index.js
@@ -1,41 +1,37 @@
-const fs = require('fs');
-
 module.exports = (api, options, rootOptions) => {
-  // 安装 svg-sprite-loader
+
   api.extendPackage({
     devDependencies: {
-      'svg-sprite-loader': '^4.1.6'
-    }
+      'svg-sprite-loader': '^5.1.1',
+    },
   });
 
   api.render('./template');
 
-  // 修改 main.js
   try {
-    const mainPath = api.resolve('src/main.js');
-    const mainExists = fs.existsSync(mainPath);
-    if (!mainExists) {
-      throw new Error('No entry found!');
-    }
-
-    api.injectImports('src/main.js', [`import '@/svg';`]);
-
-    try {
-      api.onCreateComplete(() => {
-        const injectLines = `\nimport SvgIcon from '@/components/SvgIcon';\n\nVue.component('SvgIcon', SvgIcon);`;
-        let contentMain = fs.readFileSync(mainPath, { encoding: 'utf-8' });
-        const lines = contentMain.split(/\r?\n/g).reverse();
-        const lastImportIndex = lines.findIndex(line => line.match(/^import/));
-        lines[lastImportIndex] += injectLines;
-        contentMain = lines.reverse().join('\n');
-        fs.writeFileSync(mainPath, contentMain, { encoding: 'utf-8' });
-      })
-    } catch (error) {
-      api.exitLog(error);
-    }
-    
+    api.injectImports(api.entryFile, [
+      `import '@/svg';`,
+      `import SvgIcon from '@/components/SvgIcon';`,
+    ]);
   } catch (e) {
     api.exitLog('Can not find main.js!');
   }
+};
 
+module.exports.hooks = (api) => {
+  api.afterInvoke(() => {
+    const { EOL } = require('os');
+    const fs = require('fs');
+    const contentMain = fs.readFileSync(api.resolve(api.entryFile), {
+      encoding: 'utf-8',
+    });
+    const lines = contentMain.split(/\r?\n/g);
+
+    const renderIndex = lines.findIndex((line) =>
+      line.match(/import\sSvgIcon\sfrom\s\'@\/components\/SvgIcon\'/)
+    );
+    lines[renderIndex] += `\nVue.component('SvgIcon', SvgIcon);`;
+
+    fs.writeFileSync(api.entryFile, lines.join(EOL), { encoding: 'utf-8' });
+  });
 };
